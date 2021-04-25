4fb7dde45fa571e3e59a0f8d4ff544e2e93eafbf
Spawn Colen <scolen@un-e2.kzn.21-school.ru>
2021-04-25 20:37:43 +0300

    finish libasm

--- ft_read.s
+++ ft_read.s
@@ -1,15 +1,18 @@
 global _ft_read
+extern ___error
 
 section .text
 
 _ft_read:
-        mov r10, rdx
-        mov rax, 0x2000003
-        syscall
-        jc __error
-        mov rax, r10
-        ret
-__error:
-        mov rax, -1
-        ; call __error                    ; записываю ошибку в errno
-        ret
+        mov rax, 0x2000003              ; кладу в rax вызов read
+        syscall                         ; syscall примет в себя rax
+        jc __error                      ; если ошибка
+        ret                             ; return (len_read)
+__error:                                ; в случае ошибки
+        push r10                        ; сохраняем регистр r10
+        mov r10, rax                    ; в r10 кладем длину строки
+        call ___error                   ; вызываем error
+        mov [rax], r10                  ; разыменовываем rax, и кладем туда что хранится в r10
+        pop r10                         ; достаем r10
+        mov rax, -1                     ; rax = -1
+        ret                             ; возвращаем rax, который = -1
--- ft_write.s
+++ ft_write.s
@@ -1,17 +1,19 @@
 global _ft_write
-extern __error
+extern ___error
 
 section .text
 
 _ft_write:
-        mov r10, rdx                    ; запоминаю параметр длины в ненужный регистр
-        mov rax, 0x2000004              ; rax приравниваю rax 4 вызов это write
-        syscall                         ; syscall обрабатывает rax, в котором записан 4 вызов
-        jc __error                       ; если syscall == 1, то залетаем  error
-        mov rax, r10                    ; возвращаемому значению присваеваем кол-во выведенных элементов
-        ret
-__error:                                 ; в случае ошибки
+        mov rax, 0x2000004              ; кладу в rax вызов write
+        syscall                         ; syscall примет в себя rax
+        jc __error                      ; если ошибка
+        ret                             ; return (len_write)
+__error:                                ; в случае ошибки
+        push r10                        ; сохраняем регистр r10
+        mov r10, rax                    ; в r10 кладем длину строки
+        call ___error                   ; вызываем error
+        mov [rax], r10                  ; разыменовываем rax, и кладем туда что хранится в r10
+        pop r10                         ; достаем r10
         mov rax, -1                     ; rax = -1
-        ; call __error                    ; записыаю ошибку в errno
         ret                             ; возвращаем rax, который = -1
     
